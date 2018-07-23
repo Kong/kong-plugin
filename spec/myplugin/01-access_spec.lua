30d39b424fe5fa8433bafc94bb7ea4d223069179
Sergio Puas <sergio@puas.cl>
2018-07-23 14:08:09 -0400

    add strategy for specs

--- spec/myplugin/01-access_spec.lua
+++ spec/myplugin/01-access_spec.lua
@@ -1,79 +1,79 @@
 local helpers = require "spec.helpers"
 
 for _, strategy in helpers.each_strategy() do
- describe("Demo-Plugin: myplugin (access)", function()
-  local client
+  describe("Demo-Plugin: myplugin (access) [#" .. strategy .. "]", function()
+    local client
 
-  setup(function()
-    local bp = helpers.get_db_utils(strategy)
+    setup(function()
+      local bp = helpers.get_db_utils(strategy)
 
-    local route1 = bp.routes:insert({
-      hosts = { "test1.com" },
-    })
-    bp.plugins:insert {
-      name = "myplugin",
-      route_id = route1.id,
-      config = {},
-    }
-
-    -- start kong
-    assert(helpers.start_kong({
-      -- use database strategy
-      database   = strategy,  
-      -- use the custom test template to create a local mock server
-      nginx_conf = "spec/fixtures/custom_nginx.template",
-      -- set the config item `custom_plugins` to make sure our plugin gets loaded
-      custom_plugins = "myplugin",
-    }))
-  end)
-
-  teardown(function()
-    helpers.stop_kong(nil, true)
-  end)
-
-  before_each(function()
-    client = helpers.proxy_client()
-  end)
-
-  after_each(function()
-    if client then client:close() end
-  end)
-
-  describe("request", function()
-    it("gets a 'hello-world' header", function()
-      local r = assert(client:send {
-        method = "GET",
-        path = "/request",  -- makes mockbin return the entire request
-        headers = {
-          host = "test1.com"
-        }
+      local route1 = bp.routes:insert({
+        hosts = { "test1.com" },
       })
-      -- validate that the request succeeded, response status 200
-      assert.response(r).has.status(200)
-      -- now check the request (as echoed by mockbin) to have the header
-      local header_value = assert.request(r).has.header("hello-world")
-      -- validate the value of that header
-      assert.equal("this is on a request", header_value)
-    end)
-  end)
+      bp.plugins:insert {
+        name = "myplugin",
+        route_id = route1.id,
+        config = {},
+      }
 
-  describe("response", function()
-    it("gets a 'bye-world' header", function()
-      local r = assert(client:send {
-        method = "GET",
-        path = "/request",  -- makes mockbin return the entire request
-        headers = {
-          host = "test1.com"
-        }
-      })
-      -- validate that the request succeeded, response status 200
-      assert.response(r).has.status(200)
-      -- now check the response to have the header
-      local header_value = assert.response(r).has.header("bye-world")
-      -- validate the value of that header
-      assert.equal("this is on the response", header_value)
+      -- start kong
+      assert(helpers.start_kong({
+        -- set the strategy
+        database   = strategy,
+        -- use the custom test template to create a local mock server
+        nginx_conf = "spec/fixtures/custom_nginx.template",
+        -- set the config item `custom_plugins` to make sure our plugin gets loaded
+        custom_plugins = "myplugin",
+      }))
     end)
-  end)
 
- end)
+    teardown(function()
+      helpers.stop_kong(nil, true)
+    end)
+
+    before_each(function()
+      client = helpers.proxy_client()
+    end)
+
+    after_each(function()
+      if client then client:close() end
+    end)
+
+    describe("request", function()
+      it("gets a 'hello-world' header", function()
+        local r = assert(client:send {
+          method = "GET",
+          path = "/request",  -- makes mockbin return the entire request
+          headers = {
+            host = "test1.com"
+          }
+        })
+        -- validate that the request succeeded, response status 200
+        assert.response(r).has.status(200)
+        -- now check the request (as echoed by mockbin) to have the header
+        local header_value = assert.request(r).has.header("hello-world")
+        -- validate the value of that header
+        assert.equal("this is on a request", header_value)
+      end)
+    end)
+
+    describe("response", function()
+      it("gets a 'bye-world' header", function()
+        local r = assert(client:send {
+          method = "GET",
+          path = "/request",  -- makes mockbin return the entire request
+          headers = {
+            host = "test1.com"
+          }
+        })
+        -- validate that the request succeeded, response status 200
+        assert.response(r).has.status(200)
+        -- now check the response to have the header
+        local header_value = assert.response(r).has.header("bye-world")
+        -- validate the value of that header
+        assert.equal("this is on the response", header_value)
+      end)
+    end)
+
+  end)
 end
