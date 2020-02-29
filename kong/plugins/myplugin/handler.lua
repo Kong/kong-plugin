bbd6227d7fcf6764bc97e92bbfb9bc5630412823
Thijs Schreijer <thijs@thijsschreijer.nl>
2020-02-29 16:48:31 -0700

    fix(versions) update some more old-version stuff

--- kong/plugins/myplugin/handler.lua
+++ kong/plugins/myplugin/handler.lua
@@ -3,24 +3,6 @@
 
 --assert(ngx.get_phase() == "timer", "The world is coming to an end!")
 
-
--- Grab pluginname from module name
-local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
-
-
-local plugin = {
-  PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
-  VERSION = "0.1",
-}
-
--- constructor
-function plugin:new()
-
-  -- do initialization here, runs in the 'init_by_lua_block', before worker processes are forked
-
-end
-
-
 ---------------------------------------------------------------------------------------------
 -- In the code below, just remove the opening brackets; `[[` to enable a specific handler
 --
@@ -29,39 +11,60 @@ end
 ---------------------------------------------------------------------------------------------
 
 
---[[ handles more initialization, but AFTER the worker process has been forked/created.
+
+local plugin = {
+  PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
+  VERSION = "0.1",
+}
+
+
+
+-- do initialization here, any module level code runs in the 'init_by_lua_block',
+-- before worker processes are forked. So anything you add here will run once,
+-- but be available in all workers.
+
+
+
+---[[ handles more initialization, but AFTER the worker process has been forked/created.
 -- It runs in the 'init_worker_by_lua_block'
 function plugin:init_worker()
 
   -- your custom code here
+  kong.log.debug("saying hi from the 'init_worker' handler")
 
 end --]]
+
 
 
 --[[ runs in the ssl_certificate_by_lua_block handler
 function plugin:certificate(plugin_conf)
 
   -- your custom code here
+  kong.log.debug("saying hi from the 'certificate' handler")
 
 end --]]
 
 
+
 --[[ runs in the 'rewrite_by_lua_block'
--- IMPORTANT: during the `rewrite` phase neither the `api` nor the `consumer` will have
--- been identified, hence this handler will only be executed if the plugin is
+-- IMPORTANT: during the `rewrite` phase neither `route`, `service`, nor `consumer`
+-- will have been identified, hence this handler will only be executed if the plugin is
 -- configured as a global plugin!
 function plugin:rewrite(plugin_conf)
 
   -- your custom code here
+  kong.log.debug("saying hi from the 'rewrite' handler")
 
 end --]]
+
 
 
 ---[[ runs in the 'access_by_lua_block'
 function plugin:access(plugin_conf)
 
   -- your custom code here
-  ngx.req.set_header("Hello-World", "this is on a request")
+  kong.log.inspect(plugin_conf)   -- check the logs for a pretty-printed config!
+  ngx.req.set_header(plugin_conf.request_header, "this is on a request")
 
 end --]]
 
@@ -70,7 +73,7 @@ end --]]
 function plugin:header_filter(plugin_conf)
 
   -- your custom code here, for example;
-  ngx.header["Bye-World"] = "this is on the response"
+  ngx.header[plugin_conf.response_header] = "this is on the response"
 
 end --]]
 
@@ -79,6 +82,7 @@ end --]]
 function plugin:body_filter(plugin_conf)
 
   -- your custom code here
+  kong.log.debug("saying hi from the 'body_filter' handler")
 
 end --]]
 
@@ -87,6 +91,7 @@ end --]]
 function plugin:log(plugin_conf)
 
   -- your custom code here
+  kong.log.debug("saying hi from the 'log' handler")
 
 end --]]
 
--- kong/plugins/myplugin/schema.lua
+++ kong/plugins/myplugin/schema.lua
@@ -1,11 +1,47 @@
-return {
-  no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
+local typedefs = require "kong.db.schema.typedefs"
+
+-- Grab pluginname from module name
+local plugin_name = ({...})[1]:match("^kong%.plugins%.([^%.]+)")
+
+local schema = {
+  name = plugin_name,
   fields = {
-    -- Describe your plugin's configuration's schema here.
-    
+    -- the 'fields' array is the top-level entry with fields defined by Kong
+    { consumer = typedefs.no_consumer },  -- this plugin cannot be configured on a consumer (typical for auth plugins)
+    { protocols = typedefs.protocols_http },
+    { config = {
+        -- The 'config' record is the custom part of the plugin schema
+        type = "record",
+        fields = {
+          -- a standard defined field (typedef), with some customizations
+          { request_header = typedefs.header_name {
+              required = true,
+              default = "Hello-World" } },
+          { response_header = typedefs.header_name {
+                required = true,
+                default = "Bye-World" } },
+          { ttl = { -- self defined field
+              type = "integer",
+              default = 600,
+              required = true,
+              gt = 0, }}, -- adding a constraint for the value
+        },
+        entity_checks = {
+          -- add some validation rules across fields
+          -- the following is silly because it is always true, since they are both required
+          { at_least_one_of = { "request_header", "response_header" }, },
+          -- We specify that both header-names cannot be the same
+          { distinct = { "response_body_element", "response_body_id_element"} },
+        },
+      },
+    },
   },
-  self_check = function(schema, plugin_t, dao, is_updating)
-    -- perform any custom verification
-    return true
-  end
 }
+
+-- run_on_first typedef/field was removed in Kong 2.x
+-- try to insert it, but simply ignore if it fails
+pcall(function()
+        table.insert(schema.fields, { run_on = typedefs.run_on_first })
+      end)
+
+return schema
