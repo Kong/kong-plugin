5dec91274649df5a22ca5cf811d37319d423017e
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-01-28 08:34:38 +0100

    chore(handler) update comment wrt versioning

--- kong/plugins/myplugin/handler.lua
+++ kong/plugins/myplugin/handler.lua
@@ -14,7 +14,7 @@
 
 local plugin = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
-  VERSION = "0.1",
+  VERSION = "0.1", -- version in X.Y.Z format. Check hybrid-mode compatibility requirements.
 }
 
 
